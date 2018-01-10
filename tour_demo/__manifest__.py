4a950a8f76bcdc3629651e69e5fe03aa3062b8cd
Micah Eschbacher <michael-e@superbrightleds.com>
2018-01-10 11:00:08 -0600

    initial commit

---
+++ tour_demo/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Tour of the Contacts App',
+    'summary': '',
+    'description': '',
+    'category': 'tour',
+    'sequence': 900,
+    'version': '0.1',
+    'depends': ['contacts'],
+    'data': [
+    ],
+    'images': [
+    ]
+}
